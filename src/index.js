a7ffae6eeb0ad01754fae924f6c01692b18436fb
Lukas Eipert <lukas.eipert@eccenca.com>
2016-07-08 13:08:39 +0200

    implement super agent global plugin

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import superagent from 'superagent';
 // load and apply RxJS plugin
 import superagentRx from 'superagent-rx';
+
+import superagentGlobalPlugin from './superagent-global-plugin';
+
 superagentRx(superagent);
+
+superagentGlobalPlugin(superagent);
 
 export default superagent;
--- test/index.js
+++ test/index.js
@@ -5,6 +5,8 @@ import should from 'ecc-test-helpers';
 // nock
 import nock from 'nock';
 nock('http://test.com').get('/').reply(200, 'OK');
+nock('http://test2.com').get('/').reply(200, 'OK');
+nock('http://test3.com').get('/').reply(200, 'OK');
 
 // import module
 import request from '../index.js';
@@ -25,4 +27,50 @@ describe('SuperAgent', () => {
                 done();
             });
     });
+
+    describe('Global Plugin Extension', () => {
+        it('should exist', () => {
+            // check object
+            should.exist(request.useForEachRequest);
+        });
+
+        it('should register a global plugin', () => {
+            // check object
+            request.useForEachRequest('testPlugin', request => {
+                request.url = 'http://test2.com';
+                return request;
+            });
+        });
+
+        it('should apply a global plugin', (done) => {
+
+            request
+                .get('http://foobar.com')
+                .observe() // this returns Rx.Observable
+                .subscribe(function(res) {
+                    should(res.text).equal('OK');
+                    done();
+                });
+
+        });
+
+        it('should unregister a global plugin', () => {
+            request.useForEachRequest('testPlugin', false);
+        });
+
+        it('should do not apply a unregistered plugin', (done) => {
+
+            request
+                .get('http://test3.com')
+                .observe() // this returns Rx.Observable
+                .subscribe(function(res) {
+                    should(res.text).equal('OK');
+                    done();
+                });
+
+        });
+
+    });
+
+
 });
