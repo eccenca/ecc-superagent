5c5b966d4a642cba966bd85cd37f64b28606a885
Lukas Eipert <lukas.eipert@eccenca.com>
2017-09-27 15:15:32 +0200

    refactor error handling and fix all test cases

--- src/superagent-http-problem.js
+++ src/superagent-http-problem.js
@@ -1,75 +1,69 @@
 import _ from 'lodash';
+import warning from 'warning';
+
+const DEPRECATION_WARNING = `@eccenca/superagent: You used .message of to access Error details.
+You should use the more detailed .title and .detail .`;
 
 function httpProblemHandler(callback, err, res) {
 
     // Adapt new errors (problem+json) to the old format and inform the user about deprecation.
     if (err) {
 
-        if (_.has(err, 'response.type') && err.response.type === 'application/problem+json') {
+        // Yay, we got a nice HTTP Problem error here
+        if (_.get(err, 'response.type') === 'application/problem+json') {
 
-            const message = {
-                get: function(t, n) {
-                    if (n === 'message') {
-                        if (__DEBUG__) {
-                            console.warn("The usage of message is DEPRECATED. The response is in the new HTTP Problem format. Please use title / detail / cause");
-                        }
-                        return err.response.body.title+'\n'+err.response.body.detail;
-                    }
-                    else {
-                        return t[n];
-                    }
-                }
-            };
-            err = new Proxy(err, message);
             err.title = err.response.body.title;
             err.detail = err.response.body.detail;
             err.statusCode = _.get(err, 'response.statusCode', undefined);
         }
-        // Convert connection aborted to a new format problem+json
-        else if (err.code === 'ECONNABORTED') {
-            // TODO: look at the repository and add more codes (CORS, conn refused ...)
-            const m = err.message;
-            err.title = 'A timeout error occurred';
-            err.detail = err.message;
-            const message = {
-                get: function(t, n) {
-                    if (n === 'message') {
-                        if (__DEBUG__) {
-                            console.warn("The usage of message is DEPRECATED. Please use title / detail / cause");
-                        }
-                        return 'A timeout error occurred'+'\n'+m;
-                    }
-                    else {
-                        return t[n];
-                    }
-                }
-            };
-            err = new Proxy(err, message);
-            _.set(err, 'response.type', 'application/problem+json');
-        }
+        //We try to fit all non-http errors in the HTTP error scheme
         else {
-            const detail = err.response.text;
-            const title = 'DEPRECATED old format response';
-            const message = {
 
-                get: function(t, n) {
-                    if (n === 'message') {
-                        if (__DEBUG__) {
-                            console.warn("The api call response is old format. That is DEPRECATED and should be updated.");
-                        }
-                        return title+"\n"+detail;
-                    }
-                    else {
-                        return t[n];
-                    }
-                }
-            };
-            err = new Proxy(err, message);
+            let detail = err.message;
+            let title = 'An Error occurred';
+
+            const responseText = _.get(err, 'response.text', '');
+            const responseMessage = _.get(err, 'response.body.message', '');
+
+            err.statusCode = _.get(err, 'response.statusCode', undefined);
+
+            // TODO: Which errors are also handled by superagent ? -> https://github.com/visionmedia/superagent
+            // Handle Superagent Timeout Errors
+            if (err.code === 'ECONNABORTED') {
+                title = 'A timeout error occurred';
+            }
+            // Handle HTTP Errors which have a body formed {message: '...'}
+            else if (!_.isEmpty(responseMessage)) {
+                detail = responseMessage;
+            }
+            // Handle plain HTTP text errors
+            else if (!_.isEmpty(responseText)) {
+                detail = responseText;
+            }
+            // Handle empty HTTP Errors with status code
+            else if (_.isNumber(err.statusCode)) {
+                detail = 'HTTP Error: ' + detail;
+            }
+
             err.title = title;
             err.detail = detail;
-            err.statusCode = _.get(err, 'response.statusCode', undefined);
-            _.set(err, 'response.type', 'application/problem+json');
+            err.message = title + '\n' + detail;
         }
+
+        _.set(err, 'response.type', 'application/problem+json');
+
+        err = new Proxy(err, {
+            get: function(t, n) {
+                if (n === 'message') {
+                    warning(!__DEBUG__, DEPRECATION_WARNING);
+                    return t.title + '\n' + t.detail;
+                }
+                else {
+                    return t[n];
+                }
+            }
+        });
+
     }
 
     callback(err, res);
